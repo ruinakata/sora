5dfe333f0186d0df70a5f2821523d474152f9de4
Fernando Catacora <nando.drw@gmail.com>
2014-08-06 18:46:39 -0500

    add loging partial

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,11 @@
 'use strict';
 
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+sora.module('sora', [
+
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
