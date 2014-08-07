a76eab87ac460952a8ca6a7536702465e3105725
Fernando Catacora <nando.drw@gmail.com>
2014-08-06 21:03:09 -0500

    first integration

--- app/js/app.js
+++ app/js/app.js
@@ -6,38 +6,8 @@ angular.module('Sora', [
 	'FbService'
 ]).
 config(['$routeProvider', function($routeProvider) {
-<<<<<<< HEAD
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
 
-
-
-
-
-
-
-$(document).ready(function() {
-
-	//GET BROWSER WINDOW HEIGHT
-	var currHeight = $(window).height();
-	//SET HEIGHT OF SIDEBAR AND CONTENT ELEMENTS
-	$('#sidebar, #content').css('height', currHeight);
-
-	//ON RESIZE OF WINDOW
-	$(window).resize(function() {
-
-		//GET NEW HEIGHT
-		var currHeight = $(window).height();	
-		//RESIZE BOTH ELEMENTS TO NEW HEIGHT
-		$('#sidebar, #content').css('height', currHeight);
-
-	});
-
-});
-=======
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtr'});
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'LoginCtr'});
   $routeProvider.otherwise({redirectTo: '/login'});
 }]);
->>>>>>> 77c2a6016127974c01200d680fed706cb9cffb09
