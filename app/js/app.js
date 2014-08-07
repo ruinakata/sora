ce3f6936c6ebdeb54f83f24035529b4981e4dfcd
Rui Nakata <rnakata@colgate.edu>
2014-08-07 17:31:47 -0500

    css

--- app/js/app.js
+++ app/js/app.js
@@ -2,14 +2,48 @@
 
 
 angular.module('Sora', [
-	'ngRoute',
-	'FbService',
-	'profile'
+  'ngRoute',
+  'FbService',
+  'profile',
+  'SoraLogin',
+  'FbService'
 ]).
 config(['$routeProvider', function($routeProvider) {
 
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtr'});
   $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'LoginCtr'});
   $routeProvider.when('/profile', {templateUrl: 'partials/profile.html', controller: 'ProfileCtr'});
-  // $routeProvider.otherwise({redirectTo: '/login'});
+  $routeProvider.otherwise({redirectTo: '/login'});
+}]).
+
+controller('MainController',['$scope','$route',function($scope,$route){
+  $scope.showLogin = false;
+  $scope.$on('$locationChangeSuccess',function(){
+    if($route.current.redirectTo){
+      if($route.current.redirectTo == '/login'){
+
+        $scope.videoClass = 'bgvid-show'
+        $scope.bodyStyle = "login-body";
+        $scope.showLogin = true;
+        console.log('showLogin:',$scope.showLogin);
+      }else{
+        $scope.videoClass = 'bgvid-hide'
+        $scope.showLogin = false;
+        $scope.bodyStyle = "app-body";
+        console.log('showLogin:',$scope.showLogin);
+      }
+    }else if($route.current.$$route.originalPath){
+      if($route.current.$$route.originalPath == '/login'){
+        $scope.videoClass = 'bgvid-show'
+        $scope.bodyStyle = "login-body";
+        $scope.showLogin = true;
+        console.log('showLogin:',$scope.showLogin);
+      } else{
+        $scope.videoClass = 'bgvid-hide'
+        $scope.showLogin = false;
+        $scope.bodyStyle = "app-body";
+        console.log('showLogin:',$scope.showLogin);
+      }
+    }
+  });
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -7,17 +7,31 @@
 // In this case it is a simple value service.
 var FbService = angular.module('FbService', []).
   factory('FbSrv',['$q',function($q){
-  	return{
-  		checkLoginState : function(){
-  			var deferred = $q.defer();
-  			FB.getLoginStatus(function(response){
-  				if(response.status){
-  					deferred.resolve(response);
-  				} else {
-  					deferred.resolve(response);
-  				}
-  			});
-  			return deferred.promise;
-  		}
-  	}
+    return{
+      checkLoginState : function(){
+        var deferred = $q.defer();
+        FB.getLoginStatus(function(response){
+          if(response.status){
+            deferred.resolve(response);
+          } else {
+            deferred.reject(response);
+          }
+        });
+        return deferred.promise;
+      },
+
+      login : function(){
+        var deferred = $q.defer();
+        FB.login(function(response){
+          if (response.status === 'connected') {
+            deferred.resolve(response);
+          } else if (response.status === 'not_authorized') {
+            deferred.resolve(response);
+          } else {
+            deferred.reject(response);
+          }
+        },{scope:"user_about_me,user_birthday,user_friends,user_hometown,user_education_history,user_photos"});
+
+      }
+    }
   }]);
