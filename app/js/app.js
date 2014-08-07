ef8a29d3e99786672fd3b83f7894f627c858cfb4
Rui Nakata <rnakata@colgate.edu>
2014-08-06 20:40:34 -0500

    some css

--- app/js/app.js
+++ app/js/app.js
@@ -14,3 +14,28 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+	//GET BROWSER WINDOW HEIGHT
+	var currHeight = $(window).height();
+	//SET HEIGHT OF SIDEBAR AND CONTENT ELEMENTS
+	$('#sidebar, #content').css('height', currHeight);
+
+	//ON RESIZE OF WINDOW
+	$(window).resize(function() {
+
+		//GET NEW HEIGHT
+		var currHeight = $(window).height();	
+		//RESIZE BOTH ELEMENTS TO NEW HEIGHT
+		$('#sidebar, #content').css('height', currHeight);
+
+	});
+
+});
