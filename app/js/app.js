77c2a6016127974c01200d680fed706cb9cffb09
Fernando Catacora <nando.drw@gmail.com>
2014-08-06 20:39:45 -0500

    start facebbok service

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 
-sora.module('sora', [
-
+angular.module('Sora', [
+	'ngRoute',
+	'FbService'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtr'});
+  $routeProvider.otherwise({redirectTo: '/login'});
 }]);
