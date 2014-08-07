495bb310626280ccc143c49b541b23d060995d41
Fernando Catacora <nando.drw@gmail.com>
2014-08-07 17:29:59 -0500

    add loging design

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,19 @@
 /* Controllers */
 
  angular.module('profile', [])
- Sora.controller('ProfileCtr', ['$scope', function($scope) {
+ .controller('ProfileCtr', ['$scope', function($scope) {
 
+  }]);
+
+ angular.module('SoraLogin', [])
+ .controller('LoginCtr', ['$scope','FbSrv', function($scope,FbSrv) {
+ 		this.login = function(){
+ 			console.log('happening!!!!');
+ 			FbSrv.login().then(function(){
+ 				console.log('good',response);
+ 			},function(response){
+ 				console.log('error',response);
+ 			});
+ 		}
   }]);
 
