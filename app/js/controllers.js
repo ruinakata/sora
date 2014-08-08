1e6ba4998af9fee74ac95e79be47351f0bcd78e0
Rui Nakata <rnakata@colgate.edu>
2014-08-08 11:58:16 -0500

    angular firebase started to use. will start working on taking my profile info out of firebase to show next

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,34 +2,67 @@
 
 /* Controllers */
 
- angular.module('profile', [])
- .controller('ProfileCtr', ['$scope', '$http', 'Facebook', 'FacebookPromises', function($scope, $http, Facebook, FacebookPromises) {
+ angular.module('profile', ['firebase'])
+ .controller('ProfileCtr', ['$scope', '$rootScope', '$firebase', '$http', 'Facebook', 'FacebookPromises', function($scope, $rootScope, $firebase, $http, Facebook, FacebookPromises) {
 		console.log("is facebook ready?", Facebook.isReady());
  		if ($scope.loggedInToFacebook) {
  			getMyFacebookInfo();
  		} else {
 	 		$scope.$watch(function() {
 	 			return $scope.loggedInToFacebook;
-	 		}, function(loggedIn) {
+	 		},
+	 		function(loggedIn) {
 	 			console.log('newVal:', loggedIn);
 	 			if (loggedIn) {
 		 			getMyFacebookInfo();
 	 			}
 	 		});
  		}
+		// var profileRef = new Firebase("https://amber-fire-4122.firebaseio.com/users/")
+		// profileRef.on('value', function(snapshot) {
+		// 	console.log("snapshot", snapshot.val());
+		// })
+
+
 
  		function getMyFacebookInfo() {
  			console.log('kicking off get my facebook info');
-	 		FacebookPromises.query('me', 'get', { fields: 'id,name,about,birthday,education,photos' })
+	 		FacebookPromises.query('me', 'get', { fields: 'id,name,about,birthday,education,photos,education' })
 	 			.then(function(response) {
-	 				$scope.profileinfo = response;
+	 				console.log(response)
+	 				// $scope.profileinfo = response;
+	 				var uniqueid = response.id;
+	 				$rootScope.facebookId = uniqueid;
+	 				var name = response.name;
+	 				var birthday = response.birthday;
+	 				var education = response.education[response.education.length-1].school.name
+	 				var photos = [];
+	 				console.log("hi")
+	 				for (var i=0; i<response.photos.data.length; i++) {
+	 					photos.push(response.photos.data[i].source);
+	 				};
+	 				// Save into firebase
+	 				var profileRef = new Firebase("https://amber-fire-4122.firebaseio.com/users/" + uniqueid);
+			 		
+			 		// angularfire!!!///////////////////////////////////////////////////////
+			 		var sync = $firebase(profileRef);
+			 		//download the data into a local object
+			 		var syncObject = sync.$asObject();
+			 		// sync the object with a three way binding, use asObject() to create a synchronized object, then call $bindTo() which binds
+			 		// it to a $scope variable 
+			 		syncObject.$bindTo($scope, 'profile');
+
+	 				var profile = { name: name, birthday: birthday, photos: photos, education: education };
+	 				profileRef.set(profile);
+			 		
+	 				
 	 			}, function(response) {
  					console.log(response);
 	 			})
  		}
- 		// $http.get('me?&fields=id,name,about,birthday,education,photos').success(function(data) 	{
- 		// 	$scope.profileinfo = data;
- 		// });
+
+
+
   }]);
 
 
