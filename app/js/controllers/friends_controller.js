6f1df61ad5d0fd96c62bc86f4bb52c5da72393a4
Rui Nakata <rnakata@colgate.edu>
2014-08-14 22:18:24 -0500

    got the friend request senders saved in scope

--- app/js/controllers/friends_controller.js
+++ app/js/controllers/friends_controller.js
@@ -5,12 +5,37 @@ Sora.controller('FriendCtr',
       function($scope, $rootScope, viewCoSrv, $firebase, $http, Facebook, FacebookPromises, $routeParams) {
         console.log("in Friend controller")
         // check if I have any friend requests that I need to respond to
-        var friendreqs;
         var ref = new Firebase("https://amber-fire-4122.firebaseio.com/friendreq/" + FacebookPromises.userId)
         ref.on('value', function(snapshot){
             console.log("snapshot is ", snapshot.val());
-            friendreqs = snapshot.val();
+            var friendreqs = snapshot.val();
+            if (friendreqs){
+                var senderarray = Object.keys(friendreqs)
+                console.log("senderararyis", senderarray)
+                var reqsenders = []
+                for (var i=0; i<senderarray.length; i++) {
+                    var senderinfo;
+                    var senderid = senderarray[i];
+                    console.log("senderid:", senderid)
+                    var status = friendreqs[senderid].status 
+                    console.log("status is", status)
+                    if (status == "pending") {
+                        console.log("in status==pending!")
+                        var senderref = new Firebase("https://amber-fire-4122.firebaseio.com/users/" + senderid)
+                        senderref.on('value', function(snapshot){
+                            senderinfo = snapshot.val();
+                            console.log("senderinfo", senderinfo)
+                            var obj = {name: senderinfo.name, profpic: senderinfo.photos[0], fbid: senderid}
+                            reqsenders.push(obj);
+                            console.log("reqsenders", reqsenders)
+                            $scope.$apply($scope.reqsenders = reqsenders)
+                        })
+                    }     
+                }
+            }
+
         })
+
         // $scope.friendstatus = viewCoSrv.otherProfInfo;
        
         // var isFriend = new Firebase("https://amber-fire-4122.firebaseio.com/friendreq/" + myid + '/' + otheruserid + '/status');
